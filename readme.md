c29c5400f8aba4901e48f3cbfd9fd709ba314b3c
zhangjunyi <58250587+junyiha@users.noreply.github.com>
2024-09-01 16:33:32 +0800

    update

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 ## 简介
 
 + 机器人相关的python程序
+
++ 依赖包
+```
+selenium
+pandas
+openpyxl
+bs4
+lxml
+```
--- src/grabBossJob.py
+++ src/grabBossJob.py
@@ -3,56 +3,133 @@ from selenium import webdriver
 import time
 import json
 import pandas as pd
+from bs4 import BeautifulSoup
+from lxml import etree
 
-driver = webdriver.Edge()
+def GrabJobV1():
+    driver = webdriver.Edge()
 
-driver.get("https://www.zhipin.com/web/geek/job?query=C%2B%2B&city=100010000")
+    driver.get("https://www.zhipin.com/web/geek/job?query=C%2B%2B&city=100010000")
 
-driver.delete_all_cookies()
+    driver.delete_all_cookies()
 
-with open('cookies.txt', 'r') as f:
-    cookies_list = json.load(f)
+    with open('cookies.txt', 'r') as f:
+        cookies_list = json.load(f)
+        
+        for cookie in cookies_list:
+            if isinstance(cookie.get('expiry'), float):
+                cookie['expiry'] = int(cookie['expiry'])
+            driver.add_cookie(cookie)
+
+    time.sleep(10)
+
+    tags_li = driver.find_elements(by=By.XPATH, value="//li[@class='job-card-wrapper']")
+
+    time.sleep(15)
+
+    job_data = []
+
+    for li in tags_li:
+        title_tag = li.find_elements(By.CSS_SELECTOR, ".job-name")  # 标题
+        area_tag = li.find_elements(By.CSS_SELECTOR, ".job-area-wrapper")  # 地区
+        salary_tag = li.find_elements(By.CSS_SELECTOR, ".salary")  # 工资
+        name_tag = li.find_elements(By.CSS_SELECTOR, ".company-name")  # 公司名称
+        desc_tag = li.find_elements(By.CSS_SELECTOR, ".info-desc")  # 详情
+
+        job_title = [i.text for i in title_tag]
+        job_area = [x.text for x in area_tag]
+        job_salary = [x.text for x in salary_tag]
+        job_name = [x.text for x in name_tag]
+        job_desc = [x.text for x in desc_tag]
+
+        print(job_title,job_area,job_salary,job_name,job_desc)
+        dict = {
+            '岗位': job_title,
+            '地址': job_area,
+            '工资': job_salary,
+            '公司名称': job_name,
+            '详情': job_desc,
+        }
+        job_data.append(dict)
+    df = pd.DataFrame(job_data)
+    df.to_excel('boss直聘爬虫.xlsx', index=False)
+
+    #  休眠10秒
+    time.sleep(10)
+
+    driver.close()
+    driver.quit()
     
-    for cookie in cookies_list:
-        if isinstance(cookie.get('expiry'), float):
-            cookie['expiry'] = int(cookie['expiry'])
-        driver.add_cookie(cookie)
 
-time.sleep(10)
+def ParseHTMLText(text):
+    root = etree.HTML(text)
+    br_tags = root.xpath('//br')
+    data = ""
+    for br in br_tags:
+        previous_text = br.tail.strip() if br.tail else "[No text after <br>]"
+        data += previous_text
+        data += "\n"
+        
+    return data
+    
+def GrabJobInformation(link_list):
+    driver = webdriver.Edge()
+    text_list = []
+    for link in link_list:
+        driver.get(link)
+        driver.delete_all_cookies()
 
-tags_li = driver.find_elements(by=By.XPATH, value="//li[@class='job-card-wrapper']")
+        with open('cookies.txt', 'r') as f:
+            cookies_list = json.load(f)
+            
+            for cookie in cookies_list:
+                if isinstance(cookie.get('expiry'), float):
+                    cookie['expiry'] = int(cookie['expiry'])
+                driver.add_cookie(cookie)
+        time.sleep(5)
+        text = driver.page_source
+        text_list.append(text)
 
-time.sleep(15)
+    data_list = []
+    for text in text_list:
+        soup = BeautifulSoup(text, 'html.parser')
+        job_sec_text = soup.find_all(class_ = 'job-sec-text')
+        data = ParseHTMLText(str(job_sec_text))
+        data_list.append(data)
+    
+    with open('jobInformation.txt', 'wb') as f:
+        for data in data_list:
+            data = data.encode()
+            f.write(data)
 
-job_data = []
+def GrabJobV2():
+    driver = webdriver.Edge()
 
-for li in tags_li:
-    title_tag = li.find_elements(By.CSS_SELECTOR, ".job-name")  # 标题
-    area_tag = li.find_elements(By.CSS_SELECTOR, ".job-area-wrapper")  # 地区
-    salary_tag = li.find_elements(By.CSS_SELECTOR, ".salary")  # 工资
-    name_tag = li.find_elements(By.CSS_SELECTOR, ".company-name")  # 公司名称
-    desc_tag = li.find_elements(By.CSS_SELECTOR, ".info-desc")  # 详情
+    driver.get("https://www.zhipin.com/web/geek/job?query=C%2B%2B&city=100010000")
 
-    job_title = [i.text for i in title_tag]
-    job_area = [x.text for x in area_tag]
-    job_salary = [x.text for x in salary_tag]
-    job_name = [x.text for x in name_tag]
-    job_desc = [x.text for x in desc_tag]
+    driver.delete_all_cookies()
 
-    print(job_title,job_area,job_salary,job_name,job_desc)
-    dict = {
-        '岗位': job_title,
-        '地址': job_area,
-        '工资': job_salary,
-        '公司名称': job_name,
-        '详情': job_desc,
-    }
-    job_data.append(dict)
-df = pd.DataFrame(job_data)
-df.to_excel('boss直聘爬虫.xlsx', index=False)
+    with open('cookies.txt', 'r') as f:
+        cookies_list = json.load(f)
+        
+        for cookie in cookies_list:
+            if isinstance(cookie.get('expiry'), float):
+                cookie['expiry'] = int(cookie['expiry'])
+            driver.add_cookie(cookie)
 
- #  休眠10秒
-time.sleep(10)
+    time.sleep(5)
+    text = driver.page_source
+    soup = BeautifulSoup(text, 'html.parser')
 
-driver.close()
-driver.quit()
+    job_card_left_elements = soup.find_all(class_ = 'job-card-left')
+    full_link_list = []
+    for element in job_card_left_elements:
+        href = element['href']
+        full_link = 'https://www.zhipin.com' + href
+        full_link_list.append(full_link)
+    
+    GrabJobInformation(full_link_list)
+
+
+GrabJobV2()
+# ParseHTMLText()
